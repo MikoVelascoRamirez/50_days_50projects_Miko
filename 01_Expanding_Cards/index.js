ca9603e43baa7dc8de52c9716d091deec95c0fc7
MikoVelascoRamirez <launchx08269@innovaccion.mx>
2023-05-06 22:35:31 -0600

    20. indicating behavior effect to opened card from disapeared to appeared.

--- 01_Expanding_Cards/index.js
+++ 01_Expanding_Cards/index.js
@@ -1,8 +1,26 @@
+const mainContainer = document.getElementById('container');
 
 mainContainer.addEventListener('click', e => {
     const parentElement = e.target.parentElement;
     const parentElementTagName = parentElement.localName;
     const figcaption = e.target.nextElementSibling;
+
+    if(!parentElement.classList.contains('opened') && parentElementTagName === 'figure'){
+        getElementOpen();
+
+        parentElement.classList.toggle('swipped');
+        parentElement.classList.remove('no-effect');
+
+        parentElement.classList.add('opened');
+        parentElement.classList.add('with-effect');
+
+        // Figcaption effect
+        figcaption.classList.toggle('hided');
+        figcaption.classList.remove('no-effect');
+
+        figcaption.classList.add('appeared');
+        figcaption.classList.add('with-effect');
+    }
 })
 
 function getElementOpen(){
