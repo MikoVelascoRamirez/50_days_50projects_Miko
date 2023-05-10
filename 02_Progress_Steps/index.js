d3f1f3520f5f3ec2c04fb0309ef7181d8e1f8115
MikoVelascoRamirez <launchx08269@innovaccion.mx>
2023-05-09 18:20:17 -0600

    4. creating fake state object to manage progression.

--- 02_Progress_Steps/index.js
+++ 02_Progress_Steps/index.js
@@ -1,2 +1,7 @@
 const containerControls = document.querySelector('.controls');
 const progressBar = document.querySelector('.progressBar');
+
+const progressBarState = {
+    actualStep: 1,
+    progress : 0
+}
