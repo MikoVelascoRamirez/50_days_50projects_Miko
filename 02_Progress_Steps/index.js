dcd25a36bb1ae2aaa0258dfe48ba5a62ffffec98
MikoVelascoRamirez <launchx08269@innovaccion.mx>
2023-05-09 18:31:19 -0600

    4.4. Creating function to manage buttons state.

--- 02_Progress_Steps/index.js
+++ 02_Progress_Steps/index.js
@@ -31,3 +31,24 @@ containerControls.addEventListener('click', e => {
     progressBar.value = progressBarState.progress;
     manageButtonsState()
 });
+
+function manageButtonsState(){
+    if(progressBarState.progress > 0 && progressBarState.progress < 100 ){
+        containerControls.children[0].classList.remove('inactive');
+        containerControls.children[0].classList.add('active');
+        containerControls.children[0].disabled = false;
+
+
+        containerControls.children[1].classList.remove('inactive');
+        containerControls.children[1].classList.add('active');
+        containerControls.children[1].disabled = false;
+    } else if(progressBarState.progress === 100){
+        containerControls.children[1].classList.remove('active');
+        containerControls.children[1].classList.add('inactive');
+        containerControls.children[1].disabled = true;
+    } else{
+        containerControls.children[0].classList.remove('active');
+        containerControls.children[0].classList.add('inactive');
+        containerControls.children[0].disabled = true;
+    }
+}
