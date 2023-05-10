f26c1d330602790b27c19550dfa0958e30898f8b
MikoVelascoRamirez <launchx08269@innovaccion.mx>
2023-05-09 18:27:51 -0600

    4.2. Managing increment/decrement of progress bar in controls buttons & transition effects.

--- 02_Progress_Steps/index.js
+++ 02_Progress_Steps/index.js
@@ -9,4 +9,22 @@ const progressBarState = {
 containerControls.addEventListener('click', e => {
     const buttonClicked = e.target.id;
     const stepsLbls = document.querySelector('.steps').children.length - 1;
+
+    if(buttonClicked === 'prev'){
+        progressBarState.progress -= 100 / stepsLbls;
+        progressBarState.actualStep -= 1;
+
+
+        getPreviousStep().classList.toggle('done');
+        getPreviousStep().classList.toggle('unfinished');
+
+
+    } else if(buttonClicked === 'next'){
+        progressBarState.progress += 100 / stepsLbls;
+        progressBarState.actualStep += 1;
+
+
+        getNextStep().classList.toggle('unfinished');
+        getNextStep().classList.toggle('done');
+    }
 });
