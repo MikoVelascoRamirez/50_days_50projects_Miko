8daa04d5518fc7bf5cfb0486740ef1126291e406
MikoVelascoRamirez <launchx08269@innovaccion.mx>
2023-05-10 14:53:56 -0600

    5.2. refactor: manageButtonsState() function.

--- 02_Progress_Steps/index.js
+++ 02_Progress_Steps/index.js
@@ -4,7 +4,7 @@ const steps = document.querySelector('.steps');
 
 const progressBarState = {
     actualStep: 1,
-    progress : 0,
+    progress: 0,
     buttonClicked: ''
 }
 
@@ -21,46 +21,43 @@ containerControls.addEventListener('click', e => {
     manageButtonsState()
 });
 
-function handleProgressState({buttonClicked, progress, actualStep}){
+function handleProgressState({ buttonClicked, progress, actualStep }) {
     const stepsLbls = steps.children.length - 1;
 
-    if(buttonClicked === '') return {prog: progress, step: actualStep};
+    if (buttonClicked === '') return { prog: progress, step: actualStep };
 
-    return buttonClicked === 'prev' 
-    ? { prog: progress - (100 / stepsLbls), step: actualStep - 1 } 
-    : { prog: progress + (100 / stepsLbls), step: actualStep + 1 }
+    return buttonClicked === 'prev'
+        ? { prog: progress - (100 / stepsLbls), step: actualStep - 1 }
+        : { prog: progress + (100 / stepsLbls), step: actualStep + 1 }
 }
 
-function changeStateClasses({buttonClicked}){
-    if(buttonClicked === 'prev') getPreviousStep().classList.replace('done', 'unfinished');
-    else if(buttonClicked === 'next') getNextStep().classList.replace('unfinished', 'done');
+function changeStateClasses({ buttonClicked }) {
+    if (buttonClicked === 'prev') getPreviousStep().classList.replace('done', 'unfinished');
+    else if (buttonClicked === 'next') getNextStep().classList.replace('unfinished', 'done');
 }
 
-function manageButtonsState(){
-    if(progressBarState.progress > 0 && progressBarState.progress < 100 ){
-        containerControls.children[0].classList.remove('inactive');
-        containerControls.children[0].classList.add('active');
-        containerControls.children[0].disabled = false;
+function manageButtonsState() {
+    const [prev, next] = containerControls.children;
+    if (progressBarState.progress > 0 && progressBarState.progress < 100) {
+        prev.classList.replace('inactive', 'active');
+        prev.disabled = false;
 
-        containerControls.children[1].classList.remove('inactive');
-        containerControls.children[1].classList.add('active');
-        containerControls.children[1].disabled = false;
-    } else if(progressBarState.progress === 100){
-        containerControls.children[1].classList.remove('active');
-        containerControls.children[1].classList.add('inactive');
-        containerControls.children[1].disabled = true;
-    } else{
-        containerControls.children[0].classList.remove('active');
-        containerControls.children[0].classList.add('inactive');
-        containerControls.children[0].disabled = true;
+        next.classList.replace('inactive', 'active');
+        next.disabled = false;
+    } else if (progressBarState.progress === 100) {
+        next.classList.replace('active', 'inactive');
+        next.disabled = true;
+    } else {
+        prev.classList.replace('active', 'inactive');
+        prev.disabled = true;
     }
 }
 
-function getNextStep(){
+function getNextStep() {
     return steps.querySelector(':not(.done)');
 }
 
-function getPreviousStep(){
+function getPreviousStep() {
     const labelsDone = steps.querySelectorAll(':not(.unfinished)');
-    return [...labelsDone][labelsDone.length-1];
+    return [...labelsDone][labelsDone.length - 1];
 }
